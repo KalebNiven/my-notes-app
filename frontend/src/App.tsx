26d475c596e9b0978a4c109bf19e4e1e301ace3a
Kaleb Niven <kaleb.niven7@gmail.com>
2024-05-13 17:49:11 -0500

    update frontend app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import NoteList from './components/NoteList';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 import Signup from './components/Signup';
 import './App.css'
 import SignIn from './components/Signin';
 
+
+// Middleware
+import { PrivateRoutes } from './middleware/PrivateRoutes';
+import { GuestRoutes } from './middleware/GuestRoutes';
+
+
+
 const App: React.FC = () => {
 
-  const token = localStorage.getItem('token');
-  const isAuthorized = token ?? false;
+
 
   return (
     <div className="py-8">
       <h1 className="text-center text-3xl font-bold mb-8">Notes App</h1>
-    
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NoteList />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<SignIn />} />
+        <Route element={<PrivateRoutes/>}>
+              <Route path='/' element={<NoteList/>} />
+          </Route>
+          <Route element={<GuestRoutes/>}>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<SignIn />} /> 
+          </Route>
+         
+          
         </Routes>
       </BrowserRouter>
     </div>
